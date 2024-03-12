7d46d7f642a6fe188633e88113707cd1a16fd3ae
MarkusIngerslev <markusolsen2004@gmail.com>
2024-03-12 21:36:28 +0100

    Tilføjet en test mappe med noget indhold for hjemmeside

---
+++ src/testingLists/activities.ts
@@ -0,0 +1,36 @@
+import images from "./images";
+
+const activityCards = [
+    {
+        imageSrc: images[0],
+        title: "Activity 1",
+        text: "Description of Activity 1",
+    },
+    {
+        imageSrc: images[1],
+        title: "Activity 2",
+        text: "Description of Activity 2",
+    },
+    {
+        imageSrc: images[2],
+        title: "Activity 3",
+        text: "Description of Activity 3",
+    },
+    {
+        imageSrc: images[4],
+        title: "Activity 4",
+        text: "Description of Activity 4",
+    },
+    {
+        imageSrc: images[0],
+        title: "Activity 5",
+        text: "Description of Activity 5",
+    },
+    {
+        imageSrc: images[3],
+        title: "Activity 6",
+        text: "Description of Activity 6",
+    },
+];
+
+export default activityCards;
