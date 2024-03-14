7dbb763ae6e72157c301631c789bd88d6970eaa5
MarkusIngerslev <markusolsen2004@gmail.com>
2024-03-14 13:33:32 +0100

    LAvet grid til at kunne holde 3 komponenter

--- src/pages/Utility.tsx
+++ src/pages/Utility.tsx
@@ -1,8 +1,24 @@
 function Utility() {
     return (
-        <div>
-            <h1>Utility</h1>
-        </div>
+        <>
+            <div className="container">
+                <div className="row row-cols-2 ">
+                    <div className="col px-0">
+                        <h5>1</h5>
+                    </div>
+                    <div className="col">
+                        <div className="row row-cols-1 row-gap-5">
+                            <div className="col px-0">
+                                <h5>2</h5>
+                            </div>
+                            <div className="col px-0">
+                                <h5>3</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
 
