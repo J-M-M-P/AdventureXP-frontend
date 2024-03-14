df8930edfbf784c10009f358d296ffb04b91695a
MarkusIngerslev <markusolsen2004@gmail.com>
2024-03-14 13:24:33 +0100

    Tilføjet route for Utility (udstyr) i App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Layout from "./components/Layout";
 import Activity from "./pages/Activity";
+import Utility from "./pages/Utility";
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/activity" element={<Activity />} />
+                    <Route path="/utility" element={<Utility />} />
                     <Route path="*" element={<h1>Not Found</h1>} />
                 </Routes>
             </Layout>
---
+++ src/pages/Utility.tsx
@@ -0,0 +1,9 @@
+function Utility() {
+    return (
+        <div>
+            <h1>Utility</h1>
+        </div>
+    );
+}
+
+export default Utility;
