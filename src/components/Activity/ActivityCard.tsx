848bbbd19368209b9b1d830e7595670a51c58e71
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2024-03-18 14:59:47 +0100

    Data hentes fra backend og indsættes i frontend. Knapper fører videre til aktiviteter-side. Mangler at gemme privat eller erhverv, så der er adgang til det i reservations. Der skal vel også gemmes, hvilken aktivitet, de har klikket på?

--- src/components/Activity/ActivityCard.tsx
+++ src/components/Activity/ActivityCard.tsx
@@ -1,16 +1,44 @@
+import { useNavigate } from "react-router-dom";
+
+
 interface ActivityCardProps {
-    imageSrc: string;
-    title: string;
+    image: string;
+    activityName: string;
     description: string;
     ageLimit: number;
     participantLimit: number;
 }
 
-export default function ActivityCard({ imageSrc, title, description, ageLimit, participantLimit }: ActivityCardProps) {
+export default function ActivityCard({
+    image,
+    activityName,
+    description,
+    ageLimit,
+    participantLimit,
+}: ActivityCardProps) {
+    const navigate = useNavigate();
+
+    const handleCompanyClick = () => {
+        console.log("Book erhverv " + activityName);
+        navigate("/reservation");
+    };
+
+    const handlePrivateClick = () => {
+        console.log("Book privat " + activityName);
+        navigate("/reservation");
+    };
+
     return (
         <>
             <div className="card">
-                <h3>Hello</h3>
+                <h3>{image}</h3>
+                <h3>{activityName}</h3>
+                <h3>{description}</h3>
+                <h3>Aldersgrænse: {ageLimit} år</h3>
+                <h3>Max antal deltagere: {participantLimit}</h3>
+                <p>Reservation</p>
+                <button onClick={handleCompanyClick}>Erhverv</button>
+                <button onClick={handlePrivateClick}>Privat</button>
             </div>
         </>
     );
