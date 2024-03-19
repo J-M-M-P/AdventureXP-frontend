91a8626666e1310e0efb8eb785dda535da0cff85
Jon <lundbyjon@hotmail.com>
2024-03-19 23:41:21 +0100

    user login gets saved in usestate and logged in console

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,12 +1,40 @@
-export default function login() {
+import { useState } from "react";
+
+export default function Login() {
+    const [user, setUser] = useState({ username: "", password: "" });
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log(user);
+
+        // const formData = new FormData(e.currentTarget);
+        // console.log(formData.get("username") + " " + formData.get("password"));
+        // const user1 = Object.fromEntries(formData);
+        // console.log(user1.username + " " + user1.password);
+    };
+
     return (
         <>
             <h1 className="text-center">Login</h1>
             <div className="container d-flex justify-content-center">
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="mb-3">
-                        <input type="text" className="form-control mb-2" id="loginUsername" placeholder="Username..." />
-                        <input type="password" className="form-control mb-2" id="loginPassword" placeholder="Password..." />
+                        <input
+                            type="text"
+                            className="form-control mb-2"
+                            id="loginUsername"
+                            placeholder="Brugernavn..."
+                            value={user.username}
+                            onChange={(e) => setUser((prev) => ({ ...prev, username: e.target.value }))}
+                        />
+                        <input
+                            type="password"
+                            className="form-control mb-2"
+                            id="loginPassword"
+                            placeholder="Adgangskode..."
+                            value={user.password}
+                            onChange={(e) => setUser((prev) => ({ ...prev, password: e.target.value }))}
+                        />
                         <button type="submit" className="btn btn-dark w-100 mb-2">
                             Login
                         </button>
