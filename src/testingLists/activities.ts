c3488c6c0d743ec8d850b0e9cab5f427ba0e6212
MudD3RTv <markusolsen2004@gmail.com>
2024-03-19 09:49:09 +0100

    Ændret i test data til ved fejl fetch

--- src/testingLists/activities.ts
+++ src/testingLists/activities.ts
@@ -1,51 +1,51 @@
-//import images from "./images";
-import { getActivities } from "../service/apiFacade";
+import images from "./images";
+// import { getActivities } from "../service/apiFacade";
 
-    const activityCards = await getActivities();
+// const activityCards = await getActivities();
 
-// const activityCards = [
-//     {
-//         image: images[0],
-//         title: "Activity 1",
-//         description: "Description of Activity 1",
-//         ageLimit: 12,
-//         participantLimit: 10,
-//     },
-//     {
-//         image: images[1],
-//         title: "Activity 2",
-//         description: "Description of Activity 2",
-//         ageLimit: 12,
-//         participantLimit: 15,
-//     },
-//     {
-//         image: images[2],
-//         title: "Activity 3",
-//         description: "Description of Activity 3",
-//         ageLimit: 15,
-//         participantLimit: 6,
-//     },
-//     {
-//         image: images[4],
-//         title: "Activity 4",
-//         description: "Description of Activity 4",
-//         ageLimit: 15,
-//         participantLimit: 20,
-//     },
-//     {
-//         image: images[0],
-//         title: "Activity 5",
-//         description: "Description of Activity 5",
-//         ageLimit: 18,
-//         participantLimit: 20,
-//     },
-//     {
-//         image: images[3],
-//         title: "Activity 6",
-//         description: "Description of Activity 6",
-//         ageLimit: 21,
-//         participantLimit: 12,
-//     },
-// ];
+const activityCards = [
+    {
+        image: images[0],
+        title: "Activity 1",
+        description: "Description of Activity 1",
+        ageLimit: 12,
+        participantLimit: 10,
+    },
+    {
+        image: images[1],
+        title: "Activity 2",
+        description: "Description of Activity 2",
+        ageLimit: 12,
+        participantLimit: 15,
+    },
+    {
+        image: images[2],
+        title: "Activity 3",
+        description: "Description of Activity 3",
+        ageLimit: 15,
+        participantLimit: 6,
+    },
+    {
+        image: images[4],
+        title: "Activity 4",
+        description: "Description of Activity 4",
+        ageLimit: 15,
+        participantLimit: 20,
+    },
+    {
+        image: images[0],
+        title: "Activity 5",
+        description: "Description of Activity 5",
+        ageLimit: 18,
+        participantLimit: 20,
+    },
+    {
+        image: images[3],
+        title: "Activity 6",
+        description: "Description of Activity 6",
+        ageLimit: 21,
+        participantLimit: 12,
+    },
+];
 
 export default activityCards;
