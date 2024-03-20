e92f5f35c1c28ccd4dcd00fbec2ac6479e507c35
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2024-03-20 03:57:12 +0100

    Ændring i testingLists

--- src/testingLists/activities.ts
+++ src/testingLists/activities.ts
@@ -5,42 +5,42 @@ import images from "./images";
 
 const activityCards = [
     {
-        image: images[0],
+        imageSrc: images[0],
         title: "Activity 1",
         description: "Description of Activity 1",
         ageLimit: 12,
         participantLimit: 10,
     },
     {
-        image: images[1],
+        imageSrc: images[1],
         title: "Activity 2",
         description: "Description of Activity 2",
         ageLimit: 12,
         participantLimit: 15,
     },
     {
-        image: images[2],
+        imageSrc: images[2],
         title: "Activity 3",
         description: "Description of Activity 3",
         ageLimit: 15,
         participantLimit: 6,
     },
     {
-        image: images[4],
+        imageSrc: images[4],
         title: "Activity 4",
         description: "Description of Activity 4",
         ageLimit: 15,
         participantLimit: 20,
     },
     {
-        image: images[0],
+        imageSrc: images[0],
         title: "Activity 5",
         description: "Description of Activity 5",
         ageLimit: 18,
         participantLimit: 20,
     },
     {
-        image: images[3],
+        imageSrc: images[3],
         title: "Activity 6",
         description: "Description of Activity 6",
         ageLimit: 21,
